5f1a196611631dd04afd26412f514143bf2d0d7d
JunkiKim <153966049+TAEB0702@users.noreply.github.com>
2025-12-09 08:51:40 +0900

    version 업 테스트

--- JJK_Item Manager/JKK_Item Manager.lua
+++ JJK_Item Manager/JKK_Item Manager.lua
@@ -1,7 +1,7 @@
 --========================================================
 -- @title JKK_Item Manager
 -- @author Junki Kim
--- @version 1.0.0
+-- @version 1.0.1
 --========================================================
 
 local ctx = reaper.ImGui_CreateContext('JKK_Item Manager')
