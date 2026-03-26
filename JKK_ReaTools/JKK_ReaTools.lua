e52de8efd58496a7df4435482c10e0a6483f0658
Junki Kim <153966049+junkikim-sound@users.noreply.github.com>
2026-03-26 10:59:17 +0900

    Update JKK_ReaTools.lua

--- JKK_ReaTools/JKK_ReaTools.lua
+++ JKK_ReaTools/JKK_ReaTools.lua
@@ -1,7 +1,7 @@
 --========================================================
 -- @title JKK_ReaTools
 -- @author Junki Kim
--- @version 0.8.8
+-- @version 0.8.9
 -- @provides 
 --     [nomain] Modules/JKK_ItemTool_Module.lua
 --     [nomain] Modules/JKK_TrackTool_Module.lua
